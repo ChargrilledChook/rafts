45a84d1a223343ca36a2d519ec5418539189b9e5
Dylan <58506115+ChargrilledChook@users.noreply.github.com>
2023-01-26 18:00:22 +1100

    Chapter 1 complete: Basic rack config running

--- lib/rafts.rb
+++ lib/rafts.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "rafts/version"
+require_relative "rafts/array"
 
 module Rafts
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(_env)
+      `echo debug > debug.txt`
+
+      [
+        200,
+        { "content-type" => "text/html" },
+        ["<h1>G'day big wheels 😉</h1>"]
+      ]
+    end
+  end
 end
